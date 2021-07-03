76d0288324bad355441f9b7762fed2dd00ccc2d5
Mike Conner <rockingtriangle.co@gmail.com>
2021-07-03 14:18:19 -0400

    reorganized project

---
+++ Lunch Hunch/Lunch Hunch/View Controllers/Chats/ChatsViewController.swift
@@ -0,0 +1,238 @@
+//
+//  ChatsTableViewController.swift
+//  Lunch Hunch
+//
+//  Created by Lunch Hunch Team on 6/14/21.
+//
+
+import UIKit
+import Firebase
+
+class ChatsViewController: UIViewController, PresentChatingDelegate {
+    
+    // MARK: - Properties
+    private let vm                   = ChatsViewModel()
+    private var filtred              = [RecentsViewModel]()
+    private let searchController     = UISearchController()
+    private let button               = UIButton()
+    private let badge                = UILabel(frame: .init(x: 20, y: -5, width: 22, height: 22))
+    private var selectedCell    : UserViewModel?
+    
+    // MARK: - IBOutlets
+    @IBOutlet var connectionView: UIView!
+    @IBOutlet weak var tableView: UITableView!
+    
+    // MARK: - Lifecycle
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        initView()
+        initVM()
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        badge.layoutIfNeeded()
+        tableView.reloadData()
+        let image = currentUser.image
+        button.setImage(resizeImage(image: image!, newWidth: 30, newHieght: 30), for: .normal)
+    }
+    
+    // MARK:- Init View and ViewModel
+    private func initView() {
+        setupLeftButton()
+        tableView.alpha = 0
+        tableView.tableFooterView = UIView()
+        
+        ActivityIndicator.initActivityIndecator(view: view)
+        ActivityIndicator.startAnimating()
+        
+        navigationItem.searchController = searchController
+        navigationController?.navigationBar.shadowImage = UIImage()
+        searchController.obscuresBackgroundDuringPresentation = false
+        searchController.delegate = self
+        searchController.searchResultsUpdater = self
+        
+        tabBarController?.tabBar.shadowImage = UIImage()
+        tabBarController?.tabBar.clipsToBounds = true
+    }
+    
+    private func initVM() {
+        vm.updateIndicatorClouser = { [weak self] in
+            guard let self = self else { return }
+            if !self.vm.isAnimating {
+                ActivityIndicator.stopAnimating()
+            } else {
+                ActivityIndicator.startAnimating()
+                self.tableView.alpha = 0
+            }
+        }
+        
+        vm.updateRequestsClouser = { [weak self] in
+            guard let self = self else { return }
+            let numberOfRequests = FBNetworkRequest.shared.requestsRecived.count
+            if numberOfRequests == 0 {
+                UIView.animate(withDuration: 0.2) {
+                    self.badge.alpha = 0
+                }
+            } else {
+                UIView.animate(withDuration: 0.2) {
+                    self.badge.alpha = 1
+                    self.badge.text = "\(numberOfRequests)"
+                }
+            }
+            self.badge.layoutIfNeeded()
+        }
+        
+        vm.updateTableViewClouser = { [weak self] in
+            guard let self = self else { return }
+            DispatchQueue.main.async {
+                UIView.animate(withDuration: 0.2) {
+                    self.tableView.alpha = 1
+                    self.tableView.reloadData()
+                }
+            }
+        }
+        
+        vm.updatebadgeClouser = { [weak self] in
+            guard let self = self else { return }
+            let value = self.vm.countUnreadedMessages
+            if value == 0 {
+                self.tabBarController?.tabBar.items![0].badgeValue = nil
+            } else {
+                self.tabBarController?.tabBar.items![0].badgeValue = "\(value)"
+            }
+        }
+        
+        vm.initFetchUnreadMessages()
+        vm.initFetchRequests()
+        vm.initFetchMessages()
+    }
+    
+    // MARK:- Actions
+    @objc private func requestsPressed() {
+        performSegue(withIdentifier: "ToSettingsVC", sender: self)
+    }
+    
+    @IBAction func friendsPressd(_ sender: UIBarButtonItem) {
+        let storyboard = UIStoryboard.init(name: "Chats", bundle: nil)
+        let vc = storyboard.instantiateViewController(withIdentifier: "friends") as! FriendsViewController
+        vc.delegate = self
+        present(UINavigationController(rootViewController: vc), animated: true)
+    }
+    
+    func selectedCell(user: UserViewModel) {
+        selectedCell = user
+        performSegue(withIdentifier: "ChatsToChating", sender: self)
+    }
+        
+    // Setup left button bar
+    private func setupLeftButton() {
+        let requestView                                              = UIView(frame: .init(x: 0, y: 0, width: 30, height: 30))
+        badge.alpha                                                  = 0
+        badge.textColor                                              = .white
+        badge.font                                                   = UIFont.systemFont(ofSize: 13)
+        badge.textAlignment                                          = .center
+        badge.backgroundColor                                        = .systemRed
+        badge.layer.cornerRadius                                     = 11
+        badge.layer.masksToBounds                                    = true
+        badge.adjustsFontSizeToFitWidth                              = true
+        
+        button.center                                                = requestView.center
+        button.translatesAutoresizingMaskIntoConstraints             = false
+        button.layer.cornerRadius                                    = 15
+        button.layer.masksToBounds                                   = true
+        button.heightAnchor.constraint(equalToConstant: 30).isActive = true
+        button.widthAnchor.constraint(equalToConstant: 30).isActive  = true
+        
+        if UserDefaults.standard.string(forKey: "id") == Auth.auth().currentUser!.uid {
+            let image = UIImage(data: UserDefaults.standard.data(forKey: "image")!)
+            button.setImage(resizeImage(image: image!, newWidth: 30, newHieght: 30), for: .normal)
+        }
+        
+        requestView.addSubview(button)
+        requestView.addSubview(badge)
+        let lefButton = UIBarButtonItem(customView: requestView)
+        button.addTarget(self, action: #selector(requestsPressed), for: .touchUpInside)
+        navigationItem.leftBarButtonItem = lefButton
+    }
+    
+    func resizeImage(image: UIImage, newWidth: CGFloat, newHieght: CGFloat) -> UIImage {
+        UIGraphicsBeginImageContext(CGSize(width: newWidth, height: newHieght))
+        image.draw(in: CGRect(x: 0, y: 0,width: newWidth, height: newHieght))
+        let newImage = UIGraphicsGetImageFromCurrentImageContext()
+        return newImage!.withRenderingMode(.alwaysOriginal)
+    }
+    
+}
+
+// MARK:- Setup TableView Datasource
+extension ChatsViewController: UITableViewDelegate, UITableViewDataSource {
+    
+    func numberOfSections(in tableView: UITableView) -> Int {  return vm.numberOfSection }
+    func tableView(_ tableView: UITableView, viewForHeaderInSection section: Int) -> UIView? { return vm.isReachable ? UIView() : connectionView }
+    func tableView(_ tableView: UITableView, heightForHeaderInSection section: Int) -> CGFloat { return 25 }
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat { return 75 }
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int { return vm.numberOfMessages }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: Identifiers.chats, for: indexPath) as! ChatsCell
+        cell.cellVM = vm.messageViewModel[indexPath.row]
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        vm.pressedCell(at: indexPath)
+        performSegue(withIdentifier: "ChatsToChating", sender: self)
+    }
+    
+    func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
+        if editingStyle == .delete {
+            let id = Auth.auth().currentUser?.uid
+            let chatToDelete = vm.messageViewModel[indexPath.row].uid
+            Database.database().reference().child("messages").child(id!).child(chatToDelete!).removeValue()
+            Database.database().reference().child("messages").child(chatToDelete!).child(id!).removeValue()
+            Database.database().reference().child("recent_message").child(id!).child(chatToDelete!).removeValue()
+            Database.database().reference().child("recent_message").child(chatToDelete!).child(id!).removeValue()
+            Database.database().reference().child("polling").child(id!).child(chatToDelete!).removeValue()
+            Database.database().reference().child("polling").child(chatToDelete!).child(id!).removeValue()
+            Database.database().reference().child("restaurants").child(id!).child(chatToDelete!).removeValue()
+            Database.database().reference().child("restaurants").child(chatToDelete!).child(id!).removeValue()
+            Database.database().reference().child("unread").child(id!).child(chatToDelete!).removeValue()
+            Database.database().reference().child("unread").child(chatToDelete!).child(id!).removeValue()
+            
+            vm.messageViewModel.remove(at: indexPath.row)
+            tableView.reloadData()
+        }
+    }
+    
+    // MARK: - Navigation -
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if segue.identifier == "ChatsToChating" {
+            let vc = segue.destination as! ChatingViewController
+            if selectedCell != nil {
+                vc.name = selectedCell!.name!
+                vc.uid  = selectedCell!.uid!
+                selectedCell = nil
+            } else {
+                vc.name = vm.selectedCell!.name!
+                vc.uid  = vm.selectedCell!.uid!
+            }
+        }
+    }
+}
+
+// MARK:- Search Controller Delegate
+extension ChatsViewController: UISearchControllerDelegate, UISearchResultsUpdating {
+    func updateSearchResults(for searchController: UISearchController) {
+        let text = searchController.searchBar.text?.lowercased()
+        vm.searchAbout(text: text!)
+    }
+    
+    func presentSearchController(_ searchController: UISearchController) {
+        vm.startSearching()
+    }
+    
+    func didDismissSearchController(_ searchController: UISearchController) {
+        vm.endSearching()
+    }
+}
---
+++ Lunch Hunch/Lunch Hunch/View Models/Restaurant/RestaurantVoteModel.swift
@@ -0,0 +1,41 @@
+//
+//  RestaurantVoteModel.swift
+//  Lunch Hunch
+//
+//  Created by Jacob Schvaneveldt on 6/24/21.
+//
+
+import Foundation
+
+class RestaurantVoteModel {
+    
+    // MARK: - Properties
+    static let shared = RestaurantVoteModel()
+    var sections: [[Restaurant]] {[selectedList, restaurantList]}
+    var restaurantList: [Restaurant] = [] 
+    var selectedList: [Restaurant] = []
+    
+    // MARK: - Initializer
+    private init() {}
+    
+    //MARK: - FUNCTIONS
+    func createTest(name: String, isPicked: Bool) {
+        let restaurant = Restaurant(name: name, isPicked: isPicked)
+        restaurantList.append(restaurant)
+    }
+    
+    func updateIsPicked(isPicked: Bool, restaurant: Restaurant) {
+        if isPicked && selectedList.count < 3 {
+            if let index = restaurantList.firstIndex(of: restaurant) {
+                restaurantList.remove(at: index)
+                selectedList.append(restaurant)
+            }
+        } else {
+            if let index = selectedList.firstIndex(of: restaurant) {
+                selectedList.remove(at: index)
+                restaurantList.append(restaurant)
+            }
+        }
+    }
+    
+}//End of class
